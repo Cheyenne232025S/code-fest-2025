5a37301efd14fc07d9d934bf7b293b4c8b8a960d
Cheyenne Erickson <cheyenne23@vt.edu>
2025-10-09 17:53:10 -0400

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,3 @@
+# backend API
+#  here we can clean, transform, reduce, map, filter, sort, etc. data
+# backend functions will make front end code like making diagrams, visualizations, dashboards, etc. easier and cleaner
