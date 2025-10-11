2f5b8827207e9820444e7448534878a9fadd5b67
alanL24 <alan.tuong.le@gmail.com>
2025-10-11 00:37:46 -0400

    Added react app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Header'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="header-container">
+        <h1>Testing</h1>
+        <p></p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header-container">
+      <h1>Testing</h1>
+      <p></p>
+    </div>
+  );
+}
+
+export default Header;
